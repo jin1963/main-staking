ad185be73527f4728bc4867f5258928e7c2d0c7f
KAOJNO DRAGON CHAIN <kaojino89@gmail.com>
2026-03-11 15:35:32 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+const contractAddress = "0xb3F2f75C5278E3a98c3E6b69F73768B7bd337421"; // Staking Contract Address
+const tokenAddress = "0xd479ae350dc24168e8db863c5413c35fb2044ecd"; // KJC Token Address
+const chainId = 56; // Binance Smart Chain Mainnet
